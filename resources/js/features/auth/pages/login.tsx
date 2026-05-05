0bb23c10cc013f4285a442ed81fa6c0e85de3c56
HillaryViolen <vviolenhillary@gmail.com>
2026-05-05 09:18:09 +0700

    fix: restrict login type

--- resources/js/features/auth/pages/login.tsx
+++ resources/js/features/auth/pages/login.tsx
@@ -6,10 +6,10 @@ import { Button, Checkbox, Input, InputError, Label, TextLink } from '@/componen
 import { AuthLayout } from '@/layouts';
 
 interface LoginForm {
-    [key: string]: any;
+    [key: string]: string | boolean;
     email: string;
     password: string;
-    remember: boolean;
+    remember: boolean;  
 }
 
 interface LoginProps {
